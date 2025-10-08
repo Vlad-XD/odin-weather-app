bef8eb82eff6473433fa4cd557711425f38e584d
Vlad <192461579+Vlad-XD@users.noreply.github.com>
2025-10-07 17:37:21 -0700

    Implement intial functions to fetch and process weather data

--- src/index.js
+++ src/index.js
@@ -1,2 +1,68 @@
 // import css
 import "./styles.css";
+
+// variable declarations
+const API_KEY = "A3H6XZ8XFWQCVMCY46XHXGB45";
+const location = "Las Vegas";
+
+// print data object to verify functions are working correctly
+fetchWeatherByLocation(location)
+.then((response) => {return getWeatherDataFromResponse(response);})
+.then((data) => {console.log(data);});
+
+// request data from API using a location
+async function fetchWeatherByLocation(location) {
+  const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${API_KEY}`;
+  const response = await fetch(url);
+  return response;
+}
+
+// takes a response and returns the relevant project data
+async function getWeatherDataFromResponse(response) {
+  const json = await response.json();
+
+  /* get general data:
+    - resolved address
+  */
+ const addressData = json.resolvedAddress;
+
+  /* get current forecast data:
+    - temperature
+    - description
+    - high
+    - low
+    - icon
+  */
+ const todayData = {
+    temp: json.days[0].temp,
+    desc: json.days[0].description,
+    high: json.days[0].tempmax,
+    low:  json.days[0].tempmin,
+    icon: json.days[0].icon
+ };
+
+  /* get 10 day forecast data (includes current day)
+    - date
+    - high 
+    - low 
+    - icon
+  */
+ const tenDayData = [];
+ for (let i = 0; i < 10; i++) {
+  const dayData = {
+    date: json.days[i].datetime,
+    high: json.days[i].tempmax,
+    low:  json.days[i].tempmin,
+    icon: json.days[i].icon
+  };
+  tenDayData.push(dayData);
+ }
+
+ const data = {
+  address: addressData,
+  today: todayData,
+  tenDay: tenDayData
+ };
+
+  return data;
+}
